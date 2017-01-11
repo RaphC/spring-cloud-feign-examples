c686684a41eacfb2e7b4d3fe434d3d7fa1dfaca1
RaphC <RaphC@users.noreply.github.com>
2017-01-11 13:45:18 +0100

    Update SpringCloudFeignClientHystrixDisabledTest.java

--- src/test/java/org/perso/rcr/spring/cloud/feign/SpringCloudFeignClientHystrixDisabledTest.java
+++ src/test/java/org/perso/rcr/spring/cloud/feign/SpringCloudFeignClientHystrixDisabledTest.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package org.perso.rcr.netflix.feign.support;
+package org.perso.rcr.spring.cloud.feign;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
