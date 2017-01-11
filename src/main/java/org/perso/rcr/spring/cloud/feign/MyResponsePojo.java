3873f5c1c99f882c81810af4848f7516e50e9608
RaphC <RaphC@users.noreply.github.com>
2017-01-11 13:37:34 +0100

    Create MyResponsePojo.java

---
+++ src/main/java/org/perso/rcr/spring/cloud/feign/MyResponsePojo.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.perso.rcr.spring.cloud.feign;
+
+import java.io.Serializable;
+
+import com.bouygtel.swad.web.representation.BaseObject;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author rachaumi
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MyResponsePojo extends BaseObject implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -990220172421437883L;
+
+	  @JsonProperty(value="noComptePayeur")
+	  private String noComptePayeur;
+	  
+	  @JsonProperty(value="noReference")
+	  private String noReference;
+
+	  /**
+	   * 
+	   */
+	  @JsonProperty(value="noComptePayeur")
+	  public String getNoComptePayeur() {
+	    return noComptePayeur;
+	  }
+	  public void setNoComptePayeur(String noComptePayeur) {
+	    this.noComptePayeur = noComptePayeur;
+	  }
+	  public MyResponsePojo withNoComptePayeur(String noComptePayeur) {
+	    this.noComptePayeur = noComptePayeur;
+	    return this;
+	  }
+
+	  
+	  /**
+	   * 
+	   */
+	  @JsonProperty(value="noReference")
+	  public String getNoReference() {
+	    return noReference;
+	  }
+	  public void setNoReference(String noReference) {
+	    this.noReference = noReference;
+	  }
+	  public MyResponsePojo withNoReference(String noReference) {
+	    this.noReference = noReference;
+	    return this;
+	  }
+}
